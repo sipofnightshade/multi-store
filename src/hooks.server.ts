065be88a9c31d97d20377f9ae736c798dcc4b3f0
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2024-02-07 12:42:39 -0400

    updated to use supabase ssr instead of auth helpers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,30 @@
-// src/hooks.server.ts
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { createServerClient } from '@supabase/ssr';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		event
+	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+		cookies: {
+			get: (key) => event.cookies.get(key),
+			/**
+			 * Note: You have to add the `path` variable to the
+			 * set and remove method due to sveltekit's cookie API
+			 * requiring this to be set, setting the path to an empty string
+			 * will replicate previous/standard behaviour (https://kit.svelte.dev/docs/types#public-types-cookies)
+			 */
+			set: (key, value, options) => {
+				event.cookies.set(key, value, { ...options, path: '/' });
+			},
+			remove: (key, options) => {
+				event.cookies.delete(key, { ...options, path: '/' });
+			}
+		}
 	});
 
 	/**
-	 * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
+	 * a little helper that is written for convenience so that instead
+	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
+	 * you just call this `await getSession()`
 	 */
 	event.locals.getSession = async () => {
 		const {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,26 @@
 <!-- src/routes/+page.svelte -->
 <script lang="ts">
-	import { Auth } from '@supabase/auth-ui-svelte';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
+  import { Auth } from '@supabase/auth-ui-svelte';
+  import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-	export let data;
+  export let data;
+
+  let { supabase, url } = data;
+  $: ({ supabase, url } = data);
 </script>
 
 <svelte:head>
-	<title>User Management</title>
+  <title>User Management</title>
 </svelte:head>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/auth/callback`}
-			showLinks={false}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
-		/>
-	</div>
+<div class="flex">
+  <div class="form-widget">
+    <Auth
+      supabaseClient={supabase}
+      view="magic_link"
+      redirectTo={`${url}/auth/callback`}
+      showLinks={false}
+      appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+    />
+  </div>
 </div>
