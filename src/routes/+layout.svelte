c8cff9e4aedcc20870df5e4699c6ba346affae35
Gregory McMillan <50713237+gregorymcmillan@users.noreply.github.com>
2024-02-07 10:48:45 -0400

    setup tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,29 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
-	import '../app.css';
-	import { invalidate } from '$app/navigation';
-	import { onMount } from 'svelte';
+  import '../app.pcss';
+  import { invalidate } from '$app/navigation';
+  import { onMount } from 'svelte';
 
-	export let data;
+  export let data;
 
-	let { supabase, session } = data;
-	$: ({ supabase, session } = data);
+  let { supabase, session } = data;
+  $: ({ supabase, session } = data);
 
-	onMount(() => {
-		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
-			if (_session?.expires_at !== session?.expires_at) {
-				invalidate('supabase:auth');
-			}
-		});
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
 
-		return () => data.subscription.unsubscribe();
-	});
+    return () => data.subscription.unsubscribe();
+  });
 </script>
 
 <svelte:head>
-	<title>User Management</title>
+  <title>User Management</title>
 </svelte:head>
 
 <div class="container" style="padding: 50px 0 100px 0">
-	<slot />
+  <slot />
 </div>
